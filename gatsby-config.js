8bfca645ce6b69592b054c9e56678da5f1ae7707
8 <8ukkari@gmail.com>
2019-09-01 11:58:59 +0900

    fix tex setting 2

--- gatsby-config.js
+++ gatsby-config.js
@@ -133,10 +133,10 @@ module.exports = {
             }
           },
           {
-            resolve: `gatsby-remark-katex`,
+            resolve: 'gatsby-remark-katex',
             options: {
               // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
-              strict: `ignore`
+              strict: 'ignore'
             }
           }
         ]
