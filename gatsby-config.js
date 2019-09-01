58a43ead4882bf397fb80b0cf880914766988519
8 <8ukkari@gmail.com>
2019-09-01 11:54:55 +0900

    fix tex setting

--- gatsby-config.js
+++ gatsby-config.js
@@ -133,19 +133,12 @@ module.exports = {
             }
           },
           {
-            resolve: `gatsby-transformer-remark`,
+            resolve: `gatsby-remark-katex`,
             options: {
-              plugins: [
-                {
-                  resolve: `gatsby-remark-katex`,
-                  options: {
-                    // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
-                    strict: `ignore`
-                  }
-                }
-              ],
-            },
-          },
+              // Add any KaTeX options from https://github.com/KaTeX/KaTeX/blob/master/docs/options.md here
+              strict: `ignore`
+            }
+          }
         ]
       }
     },
